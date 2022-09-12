04384a1feae7d5e8702e28159317adcde4dff3ef
DicksonWachai <wachaidickson@gmail.com>
2022-09-12 04:41:17 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A quote
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	cout << "and that piece of art is useful\" \- Dora Korpar\, 2015\-10\-19 ";
+}
