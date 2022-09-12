9775cc927753cea49af482792cdd62c0da5a2d8b
DicksonWachai <wachaidickson@gmail.com>
2022-09-12 01:25:49 -0700

    Possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		putchar('0' + i);
-		if (i == 9)
+		if (i == 10)
 			break;
 		putchar(',');
 		putchar(' ');
