91a56e44b81fac1a248704139409171f29e66541
DicksonWachai <wachaidickson@gmail.com>
2022-09-12 00:49:37 -0700

    Base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,20 +4,20 @@
  *
  * Description: Base 16
  *
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 	char ch;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
 	}
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{
-		putchar(ch + '\n');
+		putchar(ch);
 	}
 	return (0);
 }
