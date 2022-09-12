72e13b88596e6a704731ddaf62219719a9958a6a
DicksonWachai <wachaidickson@gmail.com>
2022-09-12 05:20:37 -0700

    Two digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Two digit combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 10); n++)
+		{
+			putchar('0' + i);
+			putchar('0' + n);
+			if (i == n)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
