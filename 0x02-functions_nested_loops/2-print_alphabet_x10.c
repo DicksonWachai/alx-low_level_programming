639c471898a8d09f0da2ba1996fa1ce4eb40e117
DicksonWachai <wachaidickson@gmail.com>
2022-09-14 16:32:24 -0700

    Print lowercase alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints in lowercase 10times
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch * 10);
+	}
+	_putchar('\n');
+}
