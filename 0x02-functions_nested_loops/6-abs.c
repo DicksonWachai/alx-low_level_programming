196f3d07780dc60ee4f1f4ffaaaae34ffadb8a18
DicksonWachai <wachaidickson@gmail.com>
2022-09-14 17:28:39 -0700

    Absolute integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes absolute integer value
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = (-1) * n;
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(n);
+	}
+}
