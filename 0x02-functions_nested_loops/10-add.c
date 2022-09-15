cf911a4059b94a7b2cf8678e23f68f106ab0692b
DicksonWachai <wachaidickson@gmail.com>
2022-09-14 18:22:28 -0700

    Sum of integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
  * add - computes sum of integers
- *
  * @a: value of integer 
  * @b: value of integer
- *
  * Return: the result of the sum
  */
 int add(int a, int b)
 {
 	int result = a + b;
 
-	return(result);
+	return (result);
 }
