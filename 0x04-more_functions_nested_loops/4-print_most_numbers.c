38bbda9933c48f6fa119fb69959011c022ab0885
DicksonWachai <wachaidickson@gmail.com>
2022-09-15 19:04:32 -0700

    Print numbers except two and four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		if (n == 2 && n == 4)
+		if (n != 2 && n != 4)
 		{
 			continue;
+			_putchar('0' + n);
 		}
-		_putchar('0' + n);
 		_putchar('\n');
 	}
 }
