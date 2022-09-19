5627d005e35deb8df76af1cd32e998e293806f60
DicksonWachai <wachaidickson@gmail.com>
2022-09-19 10:10:19 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int length, i;
+	char *begin, *end, t;
+
+	length= strlens(s);
+	begin = s;
+	end = s;
+	for (i = 0; i < length -i; i++)
+	{
+		end++;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		t = *end;
+		*end = * begin;
+		*begin = t;
+		begin++;
+		end++
+		}
+}
