51deec100d06a612968ec0753c46436c7e90a028
DicksonWachai <wachaidickson@gmail.com>
2022-09-19 10:12:38 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,7 @@ void print_rev(char *s)
 	int length, i;
 	char *begin, *end, t;
 
-	length= strlens(s);
+	length= _strlen(s);
 	begin = s;
 	end = s;
 	for (i = 0; i < length -i; i++)
@@ -21,6 +21,6 @@ void print_rev(char *s)
 		*end = * begin;
 		*begin = t;
 		begin++;
-		end++
-		}
+		end++;
+	}
 }
