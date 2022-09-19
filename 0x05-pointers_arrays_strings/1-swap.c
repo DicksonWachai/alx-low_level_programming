418c1a3d6685d155bc64269d12903f45638adcc0
DicksonWachai <wachaidickson@gmail.com>
2022-09-19 08:28:00 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - switcheroo
+ * @a: interger one to be swapped
+ * @b: integer two to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
