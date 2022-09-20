19ea02c75e1e0851fd949daf7cffd317248dd5d3
DicksonWachai <wachaidickson@gmail.com>
2022-09-20 01:17:33 -0700

    Half characters

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half the string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int n, length_of_the_string;
+
+	length_of_the_string = strlen(str);
+	n = (length_of_the_string) / 2;
+	while (n != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (n % 2 != 0)
+		{
+			_putchar(*str + (n % 10));
+		}
+	}
+}
