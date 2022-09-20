9a3802801829e019739add14f90c5f3144a9f5d3
DicksonWachai <wachaidickson@gmail.com>
2022-09-20 03:26:19 -0700

    Puts two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,17 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 - prints every other character
  * @str: characters to be printed
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int length, i;
+
+	length = strlen(str);
+	for (i = 0; i < length - 1; i+2)
 	{
-		if (*str == '\\')
-		{
-			break;
-		}
-		_putchar(*str);
-		str = str + 2;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
