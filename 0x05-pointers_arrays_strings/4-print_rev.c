5fa062a473654c5c89714e1fb8d0a93a97ec18eb
DicksonWachai <wachaidickson@gmail.com>
2022-09-19 18:42:26 -0700

    Reverse String

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,24 +6,7 @@
  */
 void print_rev(char *s)
 {
-	int length, i;
-	char *begin_ptr, *end_ptr, ch;
-
-	length = strlen(s);
-	begin_ptr = s;
-	end_ptr = s;
-	for (i = 0; i < length - 1;)
-	{
-		end_ptr++;
-	}
-	for (i = 0; i < length / 2; i++)
-	{
-		ch = *end_ptr;
-		*end_ptr = *begin_ptr;
-		*begin_ptr = ch;
-		begin_ptr++;
-		end_ptr--;
-	}
+	sttrev(s);
 	_putchar(*s);
 	_putchar('\n');
 }
