db499aab12b477621749f5a6964fd5203115c92a
DicksonWachai <wachaidickson@gmail.com>
2022-09-19 21:33:20 -0700

    Print reversed string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,15 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
-	_putchar(*s);
-	_putchar('\n');
+	static int i, len, temp;
+
+	len = strlen(s);
+	if (i < len / 2)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+		i++;
+		print_rev(s);
+	}
 }
