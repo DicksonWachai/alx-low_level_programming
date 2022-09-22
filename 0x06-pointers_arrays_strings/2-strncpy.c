d7dcc610e32e138646ae50dfc44ddab73243b589
DicksonWachai <wachaidickson@gmail.com>
2022-09-22 02:19:54 -0700

    String copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copies content of string
+ * @dest: string destination to be copied to
+ * @src: string source to be copied from
+ * @n: number of bytes of the source string to be copied
+ *
+ * Return: Value of copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
