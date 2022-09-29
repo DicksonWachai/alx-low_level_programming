010d59ff94d33a31400e55131088b6a31811a107
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-09-29 04:47:57 -0700

    Check Out prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,9 +7,20 @@
  */
 int is_prime_number(int n)
 {
-	if ((n <= 0) || (n == 1) || (n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0) || (n % 11 == 0))
+	if ((n <= 0) || (n == 1))
 	{
 		return (0);
 	}
-	return (1);
+	else if ((n % 2 == 0) || (n % 3 == 0))
+	{
+		return (0);
+	}
+	else if ((n % 5 == 0) || (n % 7 == 0) || (n % 11 == 0))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
