c4af7a7d395506ac0c1a85ba3d62ee6fd956abd8
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-09-29 04:35:02 -0700

    Check Out prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_prime_number - checks for prime numbers
+ * @n: integer to be checked
+ *
+ * Return: 1 if number is a prime number otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if ((n <= 0) || (n == 1) || (n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0) || (n % 11 == 0))
+	{
+		return (0);
+	}
+	return 1;
+}
