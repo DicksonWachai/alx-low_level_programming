bd6a4b14da38b56d8e9c01aac9262fa6f81a2978
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-05 05:09:49 -0700

    Two Dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	twoD = malloc(sizeof(int) * height);
+	twoD = malloc(sizeof(int *) * height);
 	if (twoD == NULL)
 	{
 		return (NULL);
