8e6c4d406d65dedb92a0afd5204b39a1a683c36a
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-07 03:28:46 -0700

    Concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,16 +4,16 @@
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: bytes allocated
+ * @n: bytes to be allocated
  *
- * Return: NULL or Concatenated string
+ * Return: NULL or concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len, i;
+	char *conct;
+	unsigned int length, i;
 
-	len = n;
+	length = n;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -24,22 +24,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; s1[i]; i++)
 	{
-		len++;
+		length++;
 	}
-	concat = malloc(sizeof(char) * (len + 1));
+	concat = malloc(sizeof(char) * (length + 1));
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
-	len = 0;
+	length = 0;
 	for (i = 0; s1[i]; i++)
 	{
-		concat[len++] = s1[index];
+		concat[length++] = s1[i];
 	}
 	for (i = 0; s2[i] && i < n; i++)
 	{
-		concat[len++] = s2[i];
+		concat[length++] = s2[i];
 	}
-	concat[len] = '\0';
+	concat[length] = '\0';
 	return (concat);
 }
