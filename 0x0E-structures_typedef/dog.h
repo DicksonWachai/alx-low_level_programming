8b7ba00e34cf5f7d29531c939cf3e7979828e82d
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-17 01:57:50 -0700

    Structure of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - Details about the dog
+ * @name: the name of the dog
+ * @age: the agae of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
