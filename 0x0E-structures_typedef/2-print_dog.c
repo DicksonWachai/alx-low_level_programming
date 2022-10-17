1b7b9856794b7ddf2d3f3fdf4f9b139b3b6cd450
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-17 02:58:22 -0700

    Print details of a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,18 +16,18 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	if (d->age == NULL)
 	{
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	if (d == NULL)
 	{
-		return (0);
+		return;
 	}
 }		
