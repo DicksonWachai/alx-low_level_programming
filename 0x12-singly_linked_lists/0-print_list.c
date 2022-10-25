54bc2bb0c2f478bf01aeec5a070509bf909ea549
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-25 05:31:33 -0700

    Print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,20 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t num = 0;
-    while (h)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)");
-        }
-        else
-        {
-            printf("[%d] %s", h->len, h->str);
-        }
-         num++;
-         h = h->next;
-    }
-    return (num);
+	size_t num = 0;
+
+	while (h)
+	{
+		if (h-str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		num++;
+		h = h->next;
+	}
+	return (num);
 }
