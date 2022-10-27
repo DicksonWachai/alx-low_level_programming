f3013aa706ec422e1a4d4b637707b72bcdf58de1
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-27 06:31:53 -0700

    Add node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,13 +9,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
-
+	
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-	newNode->n = n;
+	newNode-> = n;
 	newNode->next = *head;
 	*head = newNode;
 	return (newNode);
