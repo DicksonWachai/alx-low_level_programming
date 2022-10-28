0481b4ca6fa91541d6866abb0db251de0cdc9ba5
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-10-28 02:34:58 -0700

    Add node at nth index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert node at nth position
+ * @head: pointer to linked list
+ * @idx: nth position to add node
+ * @n: integer of new node
+ *
+ * Retun: NULL or address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+		{
+			return (NULL);
+		}
+		copy = copy->next;
+	}
+	new->next = copy->next;
+	copy->next = new;
+	return (new);
+}
