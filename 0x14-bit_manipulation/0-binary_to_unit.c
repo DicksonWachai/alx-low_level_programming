6ead78b187f956eeb4552e55945bdf76c9b3b825
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-11-02 01:00:24 -0700

    Convert number

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * binary_to_int - converts from base 2 to decimal
+ * @b: points to a string of characters2
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_int(const char *b)
+{
+	unsigned value = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		value << 1;
+		if (*b & 1)
+		{
+			value += 1;
+		}
+		b++;
+	}
+	return (value);
+}
