dd40239cce5d5b48f85389a53e34eb9664918b63
wachaidickson@gmail.com <wachaidickson@gmail.com>
2022-11-09 01:02:34 -0800

    Frees list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,4 +27,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 #endif
